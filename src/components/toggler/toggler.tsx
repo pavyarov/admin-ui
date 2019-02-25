55e196f213d446a1e1301e3856d2eca674aeb1f0
Pavel Iarov <Pavel_Iarov@epam.com>
2019-02-25 12:41:21 +0300

    US6.1: added toggle, added agents stub
    Issue: US6.1

---
+++ src/components/toggler/toggler.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { BEM } from '@redneckz/react-bem-helper';
+import { Input } from '../input';
+import { Icons } from '../icon';
+import styles from './toggler.module.scss';
+
+interface Attrs {
+  className?: string;
+  label?: string;
+  value?: string | boolean;
+  disabled?: boolean;
+  withoutControl?: boolean;
+  size?: 'small';
+  onChange?: () => void;
+}
+
+const checkbox = BEM(styles);
+
+export const Toggler = checkbox(
+  ({ className, label, value, withoutControl = false, onChange }: Attrs) => (
+    <label className={className}>
+      {!withoutControl && (
+        <React.Fragment>
+          <CheckboxInput
+            type="checkbox"
+            checked={Boolean(value)}
+            value={value}
+            onChange={onChange}
+          />
+
+          <CheckboxTogglerLabel />
+        </React.Fragment>
+      )}
+      {label && <span>{label}</span>}
+    </label>
+  ),
+);
+
+const CheckboxInput = checkbox.input(Input);
+const CheckboxTogglerLabel = checkbox.togglerLabel('span');
