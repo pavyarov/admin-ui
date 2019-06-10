afe09a8c14e7cd7d4f057651cd536692332ac0d7
Pavel Iarov <pav.yarov@gmail.com>
2019-06-10 14:30:02 +0300

    EPMDJ-35: added unregister agent modal
    Issue: EPMDJ-35

--- src/components/popup/popup.tsx
+++ src/components/popup/popup.tsx
@@ -13,28 +13,31 @@ interface Props {
   header: React.ReactNode;
   onToggle: (isOpen: boolean) => void;
   isOpen: boolean;
+  type?: 'info' | 'error';
 }
 
 const popup = BEM(styles);
 
-export const Popup = popup(({ className, header, children, onToggle, isOpen }: Props) => (
-  <div className={className}>
-    <Portal>
-      {isOpen && (
-        <div className={className}>
-          <Content>
-            <Header align="space-between">
-              {header}
-              <CloseButton onClick={() => onToggle(!isOpen)} />
-            </Header>
-            {children}
-          </Content>
-          <Fade onClick={() => onToggle(!isOpen)} />
-        </div>
-      )}
-    </Portal>
-  </div>
-));
+export const Popup = popup(
+  ({ className, header, children, onToggle, isOpen, type = 'info' }: Props) => (
+    <div className={className}>
+      <Portal>
+        {isOpen && (
+          <div className={className}>
+            <Content type={type}>
+              <Header align="space-between">
+                {header}
+                <CloseButton onClick={() => onToggle(!isOpen)} />
+              </Header>
+              {children}
+            </Content>
+            <Fade onClick={() => onToggle(!isOpen)} />
+          </div>
+        )}
+      </Portal>
+    </div>
+  ),
+);
 
 const Content = popup.content('div');
 const Header = popup.header(Panel);
