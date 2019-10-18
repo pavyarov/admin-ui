5cef7d60b1e77b021f1a5e987ab992a11f0fe354
Павел Яров <pavelarov@MBP-Pavel.moscow.epam.com>
2019-10-18 12:05:04 +0300

    added recording to active scope page

--- src/pages/agent-full-page/coverage-plugin/code-coverage-card/detailed-code-coverage-card/coverage-distribution/coverage-distribution.tsx
+++ src/pages/agent-full-page/coverage-plugin/code-coverage-card/detailed-code-coverage-card/coverage-distribution/coverage-distribution.tsx
@@ -4,6 +4,7 @@ import { BEM } from '@redneckz/react-bem-helper';
 import { Panel } from '../../../../../../layouts';
 import { Icons } from '../../../../../../components';
 import { percentFormatter } from '../../../../../../utils';
+import { useBuildVersion } from '../../../use-build-version';
 import { TestTypeSummary } from '../../../../../../types/test-type-summary';
 
 import styles from './coverage-distribution.module.scss';
@@ -11,6 +12,7 @@ import styles from './coverage-distribution.module.scss';
 interface Props {
   className?: string;
   coverageByType: { [testType: string]: TestTypeSummary };
+  showRecording?: boolean;
 }
 
 const coverageDistribution = BEM(styles);
@@ -33,37 +35,51 @@ const coverageByTypeDefaults = {
   },
 };
 
-export const CoverageDistribution = coverageDistribution(({ className, coverageByType }: Props) => {
-  return (
-    <div className={className}>
-      <DistributionContainer>
-        {Object.values({ ...coverageByTypeDefaults, ...coverageByType }).map(
-          ({ testType, coverage = 0, testCount = 0 }) => (
-            <CoverageItem key={testType}>
-              <Panel align="space-between">
-                <Panel>
-                  <Icons.Test height={16} width={16} />
-                  <ItemName>{testType.toLocaleLowerCase()}</ItemName>
+export const CoverageDistribution = coverageDistribution(
+  ({ className, coverageByType, showRecording }: Props) => {
+    const { testTypes: activeSessionTestTypes = [] } = showRecording
+      ? useBuildVersion<{ testTypes: string[] }>('/active-sessions') || {}
+      : {};
+    return (
+      <div className={className}>
+        <DistributionContainer>
+          {Object.values({ ...coverageByTypeDefaults, ...coverageByType }).map(
+            ({ testType, coverage = 0, testCount = 0 }) => (
+              <CoverageItem key={testType}>
+                <Panel align="space-between">
+                  <Panel>
+                    <Icons.Test height={16} width={16} />
+                    <ItemName>{testType.toLocaleLowerCase()}</ItemName>
+                    {showRecording && activeSessionTestTypes.includes(testType) && (
+                      <RecordingWrapper>
+                        <RecordingIcon />
+                        <RecordingText>Rec</RecordingText>
+                      </RecordingWrapper>
+                    )}
+                  </Panel>
+                  <TestsCount>{testCount}</TestsCount>
                 </Panel>
-                <TestsCount>{testCount}</TestsCount>
-              </Panel>
-              <Panel>
                 <Panel>
-                  <Icons.Function height={16} width={16} />
-                  <ItemName>Methods covered</ItemName>
+                  <Panel>
+                    <Icons.Function height={16} width={16} />
+                    <ItemName>Methods covered</ItemName>
+                  </Panel>
+                  <TestTypeCoverage>({`${percentFormatter(coverage)}%`})</TestTypeCoverage>
                 </Panel>
-                <TestTypeCoverage>({`${percentFormatter(coverage)}%`})</TestTypeCoverage>
-              </Panel>
-            </CoverageItem>
-          ),
-        )}
-      </DistributionContainer>
-    </div>
-  );
-});
+              </CoverageItem>
+            ),
+          )}
+        </DistributionContainer>
+      </div>
+    );
+  },
+);
 
 const DistributionContainer = coverageDistribution.distributionContainer('div');
 const CoverageItem = coverageDistribution.coverageItem('div');
+const RecordingWrapper = coverageDistribution.recordingWrapper(Panel);
+const RecordingIcon = coverageDistribution.recordingIcon('span');
+const RecordingText = coverageDistribution.recordingText('span');
 const ItemName = coverageDistribution.itemName('div');
 const TestsCount = coverageDistribution.testsCount('span');
 const TestTypeCoverage = coverageDistribution.testTypeCoverage('div');
