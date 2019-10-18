440b453f14c4e1d00b3eff49640e8a793ebbe096
Павел Яров <pavelarov@MBP-Pavel.moscow.epam.com>
2019-10-18 14:21:56 +0300

    EPMDJ-1597: added breadcrumbs functionality
    Issue: EPMDJ-1597

---
+++ src/utils/capitalize.ts
@@ -0,0 +1,9 @@
+export function capitalize(str?: {}): string {
+  if (str === null || str === undefined || str === '') {
+    return '';
+  }
+  if (typeof str !== 'string') {
+    return capitalize(String(str));
+  }
+  return `${str[0].toUpperCase()}${str.substr(1).toLowerCase()}`;
+}
