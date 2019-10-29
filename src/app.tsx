a2947d5c306ae6612b5754304694c5af7bf87da7
Pavel Iarov <pav.yarov@gmail.com>
2019-10-29 09:20:54 +0300

    EPMDJ-1120: added react-devirt for creating auto-test selectors
    Issue: EPMDJ-1120

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
+import { devirt } from '@redneckz/react-devirt';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { PageSwitcher } from './routes';
 import { NotificationManager } from './notification-manager';
 import { configureAxios } from './common/connection';
+
+if (process.env.NODE_ENV === 'development') {
+  devirt();
+}
 
 configureAxios();
 
