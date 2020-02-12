1cf37dde69143bd4b63c05d1f0ba6d66f698694c
pavyarov <Pavel_Iarov@epam.com>
2020-02-12 14:43:11 +0300

    EMPDJ-2318: added not implemented stub for not implemented pages
    Issue: EPMDJ-2318

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,3 +1,4 @@
 export { ManageSessionsModal } from './manage-sessions-modal';
 export { TestsToRunModal } from './tests-to-run-modal';
 export { CancelAgentRegistrationModal } from './cancel-agent-registration-modal';
+export { NotImplementedStub } from './not-implemented-stub';
--- src/pages/logs-page/logs-page.tsx
+++ src/pages/logs-page/logs-page.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { BEM } from '@redneckz/react-bem-helper';
 
-import { PageHeader, Icons } from 'components';
+import { PageHeader } from 'components';
+import { NotImplementedStub } from 'modules';
 
 import styles from './logs-page.module.scss';
 
@@ -11,16 +12,9 @@ export const LogsPage = logsPage(({ className }) => (
   <div className={className}>
     <PageHeader title="Global logs" itemsCount={0} />
     <Content>
-      <Icon height={160} width={173} />
-      <Title>There are no logs at the moment</Title>
-      <SubTitle>
-        New records will appear once you enable at least one working widget on at least one agent.
-      </SubTitle>
+      <NotImplementedStub />
     </Content>
   </div>
 ));
 
 const Content = logsPage.content('div');
-const Icon = logsPage.icon(Icons.Logs);
-const Title = logsPage.title('div');
-const SubTitle = logsPage.subTitle('div');
