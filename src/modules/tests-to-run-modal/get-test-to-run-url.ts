e1f0447a0443a7d5844ca2668361efdbbc8f1436
RomanAchilov <dakaktak95@gmail.com>
2020-02-14 11:46:04 +0300

    EPMDJ-2355: Change the view of curl comand
    Issue: EPMDJ-2355

--- src/modules/tests-to-run-modal/get-test-to-run-url.ts
+++ src/modules/tests-to-run-modal/get-test-to-run-url.ts
@@ -1,12 +1,9 @@
-export function getTestToRunURL(agentId: string, pluginId: string, agentType?: string) {
-  const adminUrl = new URL(
-    process.env.REACT_APP_ENV
-      ? `http://${window.location.host}`
-      : 'http://ecse005002af.epam.com:8090',
-  );
+import { adminUrl } from './admin-url';
 
-  const openApiUrl = 'curl -i -H "Accept: application/json" -H "Content-Type: application/json" '
-    + `-X GET ${adminUrl}api/${agentType === 'ServiceGroup' ? 'service-group' : 'agents'}/${agentId}/plugin/${pluginId}/data/tests-to-run`;
+export function getTestToRunURL(agentId: string, pluginId: string, agentType?: string) {
+  const openApiUrl = 'curl -i -H "accept: application/json" -H "content-type: application/json" '
+    + `-w "\n" -X GET ${adminUrl}api/${agentType === 'ServiceGroup'
+      ? 'service-group' : 'agents'}/${agentId}/plugin/${pluginId}/data/tests-to-run`;
 
   return openApiUrl;
 }
