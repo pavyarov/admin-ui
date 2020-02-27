b779ec493d71bd9804cb58f6f2f8d0a1f724da07
RomanAchilov <dakaktak95@gmail.com>
2020-02-27 15:46:51 +0300

    EPMDJ-2409: Correct working of the copy button on the "TestsToRun" pane
    Issue: EPMDJ-2409

--- src/modules/tests-to-run-modal/get-test-to-run-url.ts
+++ src/modules/tests-to-run-modal/get-test-to-run-url.ts
@@ -2,7 +2,7 @@ import { adminUrl } from './admin-url';
 
 export function getTestToRunURL(agentId: string, pluginId: string, agentType?: string) {
   const openApiUrl = 'curl -i -H "accept: application/json" -H "content-type: application/json" '
-    + `-w "\n" -X GET ${adminUrl}api/${agentType === 'ServiceGroup'
+    + `-w "\\n" -X GET ${adminUrl}api/${agentType === 'ServiceGroup'
       ? 'service-group' : 'agents'}/${agentId}/plugin/${pluginId}/data/tests-to-run`;
 
   return openApiUrl;
