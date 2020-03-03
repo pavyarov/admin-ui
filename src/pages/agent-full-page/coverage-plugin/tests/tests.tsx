0b341718964c099b0547fc5dee4d637f025189eb
RomanAchilov <dakaktak95@gmail.com>
2020-03-03 18:02:10 +0300

    EPMDJ-2428: Update design in test2code and scope page
    Issue: EPMDJ-2428

--- src/pages/agent-full-page/coverage-plugin/tests/tests.tsx
+++ src/pages/agent-full-page/coverage-plugin/tests/tests.tsx
@@ -4,7 +4,6 @@ import { AssociatedTests } from 'types/associated-tests';
 import { MethodCoveredByTest } from 'types/method-covered-by-test';
 import { TestDetails } from '../test-details';
 import { useBuildVersion } from '../use-build-version';
-import { CoveragePluginHeader } from '../coverage-plugin-header';
 
 export const Tests = () => {
   const testsUsages = useBuildVersion<AssociatedTests[]>('/build/tests-usages') || [];
@@ -12,7 +11,6 @@ export const Tests = () => {
   const coveredMethodsByTestType = useBuildVersion<MethodCoveredByTest[]>('/build/test-types/covered-methods') || [];
   return (
     <>
-      <CoveragePluginHeader />
       <TestDetails
         testsUsages={testsUsages}
         coveredMethodsByTest={coveredMethodsByTest}
