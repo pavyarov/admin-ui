23ab5121c7c00cf8206feeeba5375ef195b7ab0b
RomanAchilov <dakaktak95@gmail.com>
2020-03-10 19:12:39 +0300

    EPMDJ-2465: Fixed the launch "Manage Sessions" for service group
    Issue: EPMDJ-2465

--- src/modules/manage-sessions-modal/manage-session.ts
+++ src/modules/manage-sessions-modal/manage-session.ts
@@ -1,22 +1,21 @@
 import axios from 'axios';
 
-export function manageSession(
-  agentId: string,
-  pluginId: string,
-  {
-    onSuccess,
-    onError,
-  }: {
-    onSuccess?: () => void;
-    onError?: (message: string) => void;
-  },
-) {
+export function manageSession({
+  agentType, agentId, pluginId, onSuccess, onError,
+}: {
+  onSuccess?: () => void;
+  onError?: (message: string) => void;
+  agentType: string;
+  agentId: string;
+  pluginId: string;
+}) {
   return async ({ sessionId, type }: { sessionId?: string; type?: 'START' | 'STOP' }) => {
     try {
-      await axios.post(`/agents/${agentId}/plugins/${pluginId}/dispatch-action`, {
-        type,
-        payload: { sessionId },
-      });
+      await axios
+        .post(`/${agentType === 'ServiceGroup' ? 'service-groups' : 'agents'}/${agentId}/plugins/${pluginId}/dispatch-action`, {
+          type,
+          payload: { sessionId },
+        });
       onSuccess && onSuccess();
     } catch ({ response: { data: { message } = {} } = {} }) {
       onError && onError(message);
