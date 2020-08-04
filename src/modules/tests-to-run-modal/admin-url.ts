1446d76ce28e8481a70f3ca7e16068b88eb7c994
pavyarov <Pavel_Iarov@epam.com>
2020-08-04 16:09:34 +0300

    fix agent url at custom curl requests

--- src/modules/tests-to-run-modal/admin-url.ts
+++ src/modules/tests-to-run-modal/admin-url.ts
@@ -1,5 +1,5 @@
 export const adminUrl = new URL(
   process.env.REACT_APP_ENV
-    ? `http://${window.location.host}`
-    : 'http://ecse005002af.epam.com:8090',
+    ? `${window.location.protocol}://${window.location.host}`
+    : `http://${process.env.REACT_APP_API_HOST || window.location.host}`,
 );
