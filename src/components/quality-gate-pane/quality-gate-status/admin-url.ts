7684900b4562a9a1fdd0109a32dff93561806be0
pavyarov <Pavel_Iarov@epam.com>
2020-08-05 11:16:56 +0300

    fix double column at adminUrl function

--- src/components/quality-gate-pane/quality-gate-status/admin-url.ts
+++ src/components/quality-gate-pane/quality-gate-status/admin-url.ts
@@ -1,5 +1,5 @@
 export const adminUrl = new URL(
   process.env.REACT_APP_ENV
-    ? `${window.location.protocol}://${window.location.host}`
+    ? `${window.location.protocol}//${window.location.host}`
     : `http://${process.env.REACT_APP_API_HOST || window.location.host}`,
 );
