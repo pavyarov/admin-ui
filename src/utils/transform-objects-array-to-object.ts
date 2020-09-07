8d0604d2980918e579df1d39930de543d3e062a2
pavyarov <Pavel_Iarov@epam.com>
2020-09-07 14:59:28 +0300

    Refactoring: restrict keys type at transformObjectsArrayToObject

--- src/utils/transform-objects-array-to-object.ts
+++ src/utils/transform-objects-array-to-object.ts
@@ -1,2 +1,2 @@
-export const transformObjectsArrayToObject = <T, K extends keyof T>(array: T[], key: K) =>
+export const transformObjectsArrayToObject = <T, K extends keyof T>(array: T[], key: T[K] extends string ? K : never) =>
   array.reduce((acc, value) => ({ ...acc, [value[key] as any]: value }), {} as { [key: string]: T});
