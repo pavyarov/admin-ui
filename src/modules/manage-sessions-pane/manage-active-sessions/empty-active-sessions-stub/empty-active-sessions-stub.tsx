892985bcc29ee6c5b7202e8e6a72cd1559931cc5
RomanAchilov <dakaktak95@gmail.com>
2020-09-25 14:05:13 +0300

    EPMDJ-4336: Correct the centering of the stub
    Issue: EPMDJ-4336

--- src/modules/manage-sessions-pane/manage-active-sessions/empty-active-sessions-stub/empty-active-sessions-stub.tsx
+++ src/modules/manage-sessions-pane/manage-active-sessions/empty-active-sessions-stub/empty-active-sessions-stub.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { BEM } from '@redneckz/react-bem-helper';
-import { Icons } from '@drill4j/ui-kit';
+import { Icons, Panel } from '@drill4j/ui-kit';
 
 import styles from './empty-active-sessions-stub.module.scss';
 
@@ -13,9 +13,11 @@ const emptyActiveSessionsStub = BEM(styles);
 export const EmptyActiveSessionsStub = emptyActiveSessionsStub(
   ({ className }: Props) => (
     <div className={className}>
-      <Icons.Test width={120} height={134} viewBox="0 0 18 20" data-test="empty-active-sessions-stub:test-icon" />
-      <Title data-test="empty-active-sessions-stub:title">There are no active sessions</Title>
-      <Message data-test="empty-active-sessions-stub:message">You can use this menu to start new.</Message>
+      <Panel direction="column">
+        <Icons.Test width={120} height={134} viewBox="0 0 18 20" data-test="empty-active-sessions-stub:test-icon" />
+        <Title data-test="empty-active-sessions-stub:title">There are no active sessions</Title>
+        <Message data-test="empty-active-sessions-stub:message">You can use this menu to start new.</Message>
+      </Panel>
     </div>
   ),
 );
