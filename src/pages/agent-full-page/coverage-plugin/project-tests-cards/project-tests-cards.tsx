b91d98c3243ae989744450995bfe97a0d405f75a
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2020-11-02 15:34:03 +0300

    EPMDJ-4990: Remove test to run links from build tests card
    Issue: EPMDJ-4990

--- src/pages/agent-full-page/coverage-plugin/project-tests-cards/project-tests-cards.tsx
+++ src/pages/agent-full-page/coverage-plugin/project-tests-cards/project-tests-cards.tsx
@@ -1,17 +1,11 @@
 import * as React from 'react';
 import { BEM } from '@redneckz/react-bem-helper';
-import { LinkButton } from '@drill4j/ui-kit';
-import { useParams } from 'react-router-dom';
 
 import { InfoCard } from 'components';
-import { TestsToRunModal } from 'modules';
 import { percentFormatter } from 'utils';
 import { TestTypeSummary } from 'types/test-type-summary';
 import { TestSummary } from 'types/test-summary';
-import { TestTypeCount } from 'types/test-type-count';
 import { TestsInfo } from 'types/tests-info';
-import { TestsToRun } from 'types/tests-to-run';
-import { useBuildVersion } from '../use-build-version';
 
 import styles from './project-tests-cards.module.scss';
 
@@ -19,24 +13,15 @@ interface Props {
   className?: string;
   allTests: TestSummary;
   testsByType: TestTypeSummary[];
-  testsToRun?: TestTypeCount[];
-}
-
-interface TestToRunByType {
-  [key: string]: TestTypeCount;
 }
 
 const projectTestsCards = BEM(styles);
 
 export const ProjectTestsCards = projectTestsCards(
   ({
-    className, allTests, testsByType, testsToRun = [],
+    className, allTests, testsByType,
   }: Props) => {
-    const [testsFilter, setTestsFilter] = React.useState<string>('');
-    const { agentId = '', pluginId = '' } = useParams<{ agentId: string, pluginId: string }>();
     const testsInfo: TestsInfo = testsByType.reduce((test, testType) => ({ ...test, [testType.type]: testType }), {});
-    const testsToRunByType: TestToRunByType = testsToRun.reduce((test, testType) => ({ ...test, [testType.type]: testType }), {});
-    const { testTypeToNames = {} } = useBuildVersion<TestsToRun>('/build/tests-to-run') || {};
     return (
       <div className={className}>
         <InfoCard
@@ -48,34 +33,12 @@ export const ProjectTestsCards = projectTestsCards(
           label="AUTOMATED"
           covered={`${percentFormatter(testsInfo?.AUTO?.summary?.coverage?.percentage || 0)}%`}
           totalCount={testsInfo?.AUTO?.summary?.testCount}
-        >
-          {Boolean(testsToRunByType?.AUTO?.count) && (
-            <LinkButton size="small" onClick={() => setTestsFilter('auto')} data-test="info-card:link-button:auto">
-              {testsToRunByType?.AUTO?.count} to run
-            </LinkButton>
-          )}
-        </InfoCard>
+        />
         <InfoCard
           label="MANUAL"
           covered={`${percentFormatter(testsInfo?.MANUAL?.summary?.coverage?.percentage || 0)}%`}
           totalCount={testsInfo?.MANUAL?.summary?.testCount}
-        >
-          {Boolean(testsToRunByType?.MANUAL?.count) && (
-            <LinkButton size="small" onClick={() => setTestsFilter('manual')} data-test="info-card:link-button:manual">
-              {testsToRunByType?.MANUAL?.count} to run
-            </LinkButton>
-          )}
-        </InfoCard>
-        {testsFilter && (
-          <TestsToRunModal
-            isOpen={Boolean(testsFilter)}
-            onToggle={() => setTestsFilter('')}
-            testsToRun={testTypeToNames}
-            filter={testsFilter}
-            agentId={agentId}
-            pluginId={pluginId}
-          />
-        )}
+        />
       </div>
     );
   },
--- src/pages/agent-full-page/coverage-plugin/tests/tests.tsx
+++ src/pages/agent-full-page/coverage-plugin/tests/tests.tsx
@@ -4,7 +4,6 @@ import { AssociatedTests } from 'types/associated-tests';
 import { MethodCoveredByTest } from 'types/method-covered-by-test';
 import { TestSummary } from 'types/test-summary';
 import { TestTypeSummary } from 'types/test-type-summary';
-import { TestTypeCount } from 'types/test-type-count';
 import { TestDetails } from '../test-details';
 import { useBuildVersion } from '../use-build-version';
 import { ProjectTestsCards } from '../project-tests-cards';
@@ -14,10 +13,9 @@ export const Tests = () => {
   const coveredMethodsByTest = useBuildVersion<MethodCoveredByTest[]>('/build/tests/covered-methods') || [];
   const allTests = useBuildVersion<TestSummary>('/build/summary/tests/all') || {};
   const testsByType = useBuildVersion<TestTypeSummary[]>('/build/summary/tests/by-type') || [];
-  const testsToRun = useBuildVersion<TestTypeCount[]>('/build/summary/tests-to-run/by-type') || [];
   return (
     <>
-      <ProjectTestsCards allTests={allTests} testsByType={testsByType} testsToRun={testsToRun} />
+      <ProjectTestsCards allTests={allTests} testsByType={testsByType} />
       <TestDetails
         testsUsages={testsUsages}
         coveredMethodsByTest={coveredMethodsByTest}
